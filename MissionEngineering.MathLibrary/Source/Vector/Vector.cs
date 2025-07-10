2b4cbc2ea380a33ede43716c7428ff77c759cf70
MissionEngineeringSupport <support@missionengineeringsupport.co.uk>
2025-07-10 21:02:52 +0100

    First version.

---
+++ MissionEngineering.MathLibrary/Source/Vector/Vector.cs
@@ -0,0 +1,29 @@
+﻿namespace MissionEngineering.MathLibrary;
+
+public class Vector
+{
+    public double[] Data { get; set; }
+
+    public int NumberOfElements => Data.Length;
+
+    public Vector()
+    {
+        Data = Array.Empty<double>();
+    }       
+
+    public Vector(int numberOfElements)
+    {
+        Data = new double[numberOfElements];
+    }
+
+    public Vector(double[] data)
+    {
+        Data = data;
+    }
+
+     public double this[int index]
+    {
+        get => Data[index];
+        set => Data[index] = value;
+    }
+}
