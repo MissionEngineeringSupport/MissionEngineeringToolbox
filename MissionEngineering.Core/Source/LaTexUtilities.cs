cf3d998b66bea092e7577006fc758e9cdc1e36b9
Mission Engineering <support@missionengineeringsupport.co.uk>
2025-07-19 18:43:27 +0100

    5: Added automatic export to pdf from latex.

---
+++ MissionEngineering.Core/Source/LaTexUtilities.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+namespace MissionEngineering.Core;
+
+public static class LaTexUtilities
+{
+    public static void ConvertTexToPdf(string texFile)
+    {
+        LogUtilities.LogInformation("Converting Tex to PDF. Starting...");
+        LogUtilities.LogInformation("");
+
+        var path = Path.GetDirectoryName(texFile);
+        var fileNameTex = Path.GetFileName(texFile);
+
+        var pdfFile = texFile.Replace(".tex", ".pdf");
+
+        LogUtilities.LogInformation($"     Input Tex file = {texFile}");
+        LogUtilities.LogInformation($"    Output Pdf file = {pdfFile}");
+        LogUtilities.LogInformation("");
+
+        Process process = new Process();
+
+        process.StartInfo.FileName = "pdflatex.exe";
+        process.StartInfo.WorkingDirectory = path;
+        process.StartInfo.Arguments = fileNameTex;
+
+        process.Start();
+
+        process.WaitForExit();
+
+        LogUtilities.LogInformation("Converting Tex to PDF. Done.");
+        LogUtilities.LogInformation("");
+    }
+}
--- MissionEngineering.Radar.RadarDetectionModel.Console/Source/Program.cs
+++ MissionEngineering.Radar.RadarDetectionModel.Console/Source/Program.cs
@@ -5,6 +5,10 @@ namespace MissionEngineering.Simulation;
 
 public class Program
 {
+    public static string OutputFolder {  get; set; }
+
+    public static string ScenarioName { get; set; }
+
     public static RadarDetectionModelHarness RadarDetectionModelHarness { get; set; }
 
     public static RadarDetectionModelInputData RadarDetectionModelInputData { get; set; }
@@ -18,11 +22,34 @@ public class Program
     {
         GenerateDetectionModelSettings();
 
+        SetOutputFolder();
+
+        CreateLog();
+
         CreateDetectionModelHarness();
 
         RunDetectionModel();
 
         OutputData();
+    }
+
+    private static void GenerateDetectionModelSettings()
+    {
+        RadarDetectionModelInputData = RadarDetectionModelInputDataFactory.RadarDetectionModelInputData_Test_1();
+
+        ScenarioName = RadarDetectionModelInputData.RadarSystemSettings.RadarSystemName;
+    }
+
+    private static void SetOutputFolder()
+    {
+        OutputFolder = @"C:\Temp\MissionEngineeringToolbox\RadarDetectionModel\";
+    }
+
+    private static void CreateLog()
+    {
+        var logFile = Path.Combine(OutputFolder, @"{ScenarioName}.log");
+
+        LogUtilities.CreateLogger(logFile);
     }
 
     private static void CreateDetectionModelHarness()
@@ -36,11 +63,6 @@ public class Program
         };
     }
 
-    private static void GenerateDetectionModelSettings()
-    {
-        RadarDetectionModelInputData = RadarDetectionModelInputDataFactory.RadarDetectionModelInputData_Test_1();
-    }
-
     private static void RunDetectionModel()
     {
         RadarDetectionModelHarness.Run();
@@ -48,12 +70,18 @@ public class Program
 
     private static void OutputData()
     {
-        var inputDataFileName = @"C:\Temp\MissionEngineeringToolbox\RadarDetectionModel\Radar_Test_1_DetectionModel_InputData.json";
+        var outputFolder = @"C:\Temp\MissionEngineeringToolbox\RadarDetectionModel\";
+
+        var inputDataFileName = outputFolder + @"Radar_Test_1_DetectionModel_InputData.json";
 
         RadarDetectionModelInputData.WriteToJsonFile(inputDataFileName);
 
-        var outputDataFileName = @"C:\Temp\MissionEngineeringToolbox\RadarDetectionModel\Radar_Test_1_DetectionModel_OutputData.csv";
+        var outputDataFileName = outputFolder + @"Radar_Test_1_DetectionModel_OutputData.csv";
 
         RadarDetectionModelHarness.OutputDataList.WriteToCsvFile(outputDataFileName);
+
+        var texFileName = outputFolder + @"GenerateJSON.tex";
+
+        LaTexUtilities.ConvertTexToPdf(texFileName);
     }
 }
