84cdfa5ce5236be8fe25abe0590971e19bbb61fa
Mission Engineering <support@missionengineeringsupport.co.uk>
2025-07-29 21:50:53 +0100

    5: Updated harness test to also output the data to file. Checks more of the process.

--- MissionEngineering.Radar.Tests/Source/RadarDetectionModelHarnessTests.cs
+++ MissionEngineering.Radar.Tests/Source/RadarDetectionModelHarnessTests.cs
@@ -4,18 +4,26 @@
 public sealed class RadarDetectionModelHarnessTests
 {
     [TestMethod]
-    public void Update_WithInitialiseCalled_ExpectSuccess()
+    public void Run_WithValidData_ExpectSuccess()
     {
         // Arrange:
         var inputData = RadarDetectionModelHarnessInputDataFactory.Scenario_1();
 
+        var outputFolder = Environment.CurrentDirectory;
+
+        var inputFilePath = Path.Combine(outputFolder, $"{inputData.ScenarioName}_RadarDetectionModelHarness_InputData.json");
+
         var radarDetectionModelHarness = new RadarDetectionModelHarness()
         {
             InputData = inputData,
+            InputFilePath = inputFilePath,
+            OutputFolder = outputFolder
         };
-
+     
         // Act:
         radarDetectionModelHarness.Run();
+
+        radarDetectionModelHarness.OutputData();
 
         // Assert:
         Assert.AreEqual(radarDetectionModelHarness.TargetRanges.NumberOfElements, radarDetectionModelHarness.OutputDataList[0].Count);
