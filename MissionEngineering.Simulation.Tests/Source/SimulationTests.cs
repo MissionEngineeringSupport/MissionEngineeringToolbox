709ef9544936e4c2cc36ce43d0c4357641c752e6
Mission Engineering <support@missionengineeringsupport.co.uk>
2025-07-29 22:03:26 +0100

    3: Added Simulation Tests project.

---
+++ MissionEngineering.Simulation.Tests/Source/SimulationTests.cs
@@ -0,0 +1,26 @@
+﻿namespace MissionEngineering.Simulation.Tests
+{
+    [TestClass]
+    public sealed class SimulationTests
+    {
+        [TestMethod]
+        public void Run_WithValidData_ExpectSuccess()
+        {
+            // Arrange:
+            var simulationSettings = SimulationSettingsFactory.SimulationSettings_Test_1_Single();
+            var scenarioSettings = ScenarioSettingsFactory.ScenarioSettings_Test_1();
+
+            var simulationHarness = SimulationBuilder.CreateSimulationHarness();
+
+            simulationHarness.SimulationSettings = simulationSettings;
+            simulationHarness.ScenarioSettings = scenarioSettings;
+            simulationHarness.SimulationHarnessSettings.NumberOfRuns = 1;
+
+            // Act:
+            simulationHarness.Run();
+
+            // Assert:
+            Assert.AreEqual(simulationHarness.Simulation.SimulationModels.Count, 2);
+        }
+    }
+}
