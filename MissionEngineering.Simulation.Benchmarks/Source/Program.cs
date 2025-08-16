0b83d500aab9cb3f36df2cd3ea242b4fc243084b
Mission Engineering <support@missionengineeringsupport.co.uk>
2025-08-16 20:12:59 +0100

    2: Added Simulation Benchmarks console project.

---
+++ MissionEngineering.Simulation.Benchmarks/Source/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace MissionEngineering.Simulation;
+
+/// <summary>
+///
+/// </summary>
+public class Program
+{
+    public static void Main()
+    {
+        var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+    }
+}
---
+++ MissionEngineering.Simulation.Benchmarks/Source/SimulationRunner.cs
@@ -0,0 +1,53 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.VSDiagnostics;
+
+namespace MissionEngineering.Simulation;
+
+public class SimulationRunner
+{
+    public int NumberOfRuns { get; set; }
+
+    public SimulationSettings SimulationSettings { get; set; }
+
+    public ScenarioSettings ScenarioSettings { get; set; }
+
+    public ISimulationHarness SimulationHarness { get; set; }
+
+    public SimulationRunner()
+    {
+    }
+
+    [Benchmark]
+    public void Run()
+    {
+        NumberOfRuns = 1;
+
+        GenerateSimulationSettings();
+        GenerateScenarioSettings();
+
+        SimulationHarness = SimulationBuilder.CreateSimulationHarness();
+
+        SimulationHarness.SimulationSettings = SimulationSettings;
+        SimulationHarness.ScenarioSettings = ScenarioSettings;
+        SimulationHarness.SimulationHarnessSettings.NumberOfRuns = NumberOfRuns;
+
+        SimulationHarness.Run();
+    }
+
+    private void GenerateSimulationSettings()
+    {
+        if (NumberOfRuns == 1)
+        {
+            SimulationSettings = SimulationSettingsFactory.SimulationSettings_Test_1_Single();
+        }
+        else
+        {
+            SimulationSettings = SimulationSettingsFactory.SimulationSettings_Test_1_Multiple();
+        }
+    }
+
+    private void GenerateScenarioSettings()
+    {
+        ScenarioSettings = ScenarioSettingsFactory.ScenarioSettings_Test_1();
+    }
+}
